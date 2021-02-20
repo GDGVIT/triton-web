1175d733d01066bf62ae23f19929dcb61bb0701d
BRO3886 <siddverma1999@gmail.com>
2021-02-21 01:12:40 +0530

    feat(redirect): setup json based redirect
    Signed-off-by: BRO3886 <siddverma1999@gmail.com>

---
+++ workers-site/data.js
@@ -0,0 +1,16 @@
+export const redirect = {
+  afterhours: 'https://afterhours.dscvit.com',
+  community: 'https://dsc.community.dev/vellore-institute-of-technology/',
+  insta: 'https://www.instagram.com/dscvitvellore',
+  linkedin: 'https://www.linkedin.com/company/dscvit',
+  solchal:
+    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-solution-challenge-info-session/#/',
+  solutionchallengeinfo:
+    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-solution-challenge-info-session/#/',
+  twitter: 'https://twitter.com/dscvit',
+  vitcourses:
+    'https://drive.google.com/file/d/1KUBodSJGrW1JaObGYJVENxfTvbvpPZOk/view?usp=sharing',
+  wt21: 'https://womentechies.dscvit.com/',
+  wtdiscord: 'https://discord.gg/aWuNESPhNf',
+  youtube: 'https://www.youtube.com/channel/UCvT-ZJF7fXHJi9kDeCPR-zg',
+}
--- workers-site/handler.js
+++ workers-site/handler.js
@@ -1,4 +1,5 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+import { data, redirect } from './data.js'
 
 const GITHUB_USERNAME = 'GDGVIT'
 const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
@@ -33,62 +34,40 @@ async function getPageFromKV(event) {
         ...notFoundResponse,
         status: 404,
       })
-    } catch (e) { }
+    } catch (e) {}
     return new Response(e.message || e.toString(), { status: 500 })
   }
 }
 
 async function redirectGitHub(event) {
   const urlParts = event.request.url.replace(BASE_URL, '').split('/')
-  switch (urlParts[0].toLowerCase()) {
-    case 'g':
-      switch (urlParts.length) {
-        case 1:
-          return Response.redirect(GITHUB_URL, 301)
-        case 2:
-          return Response.redirect(`${GITHUB_URL}/${urlParts[1]}`, 301)
-        case 3:
-          return Response.redirect(
-            `${GITHUB_URL}/${urlParts[1]}/commit/${urlParts[2]}`,
-            301
-          )
-        case 4:
-          return Response.redirect(
-            `${GITHUB_URL}/${urlParts[1]}/issues/${urlParts[3]}`,
-            301
-          )
-      }
-      break
-    case 'afterhours':
-      return Response.redirect('https://afterhours.dscvit.com', 301)
-
-    case 'solchal':
-    case 'solutionchallengeinfo':
-      return Response.redirect(
-        'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-solution-challenge-info-session/#/',
-        301
-      )
-    case 'community':
-      return Response.redirect(
-        'https://dsc.community.dev/vellore-institute-of-technology/',
-        301
-      )
-    case 'twitter':
-      return Response.redirect('https://twitter.com/dscvit', 301)
-    case 'linkedin':
-      return Response.redirect('https://www.linkedin.com/company/dscvit', 301)
-    case 'insta':
-      return Response.redirect('https://www.instagram.com/dscvitvellore', 301)
-    case 'wt21':
-      return Response.redirect('https://womentechies.dscvit.com/', 301)
-    case 'youtube':
-      return Response.redirect('https://www.youtube.com/channel/UCvT-ZJF7fXHJi9kDeCPR-zg',301)
-    case 'wtdiscord':
-      return Response.redirect('https://discord.gg/aWuNESPhNf',301)
-    case 'vit-curriculum':
-    case 'vitcourses':
-      return Response.redirect('https://drive.google.com/file/d/1KUBodSJGrW1JaObGYJVENxfTvbvpPZOk/view?usp=sharing',301)
-    default:
-      return getPageFromKV(event)
+  if (redirect[urlParts[0]]) {
+    return Response.redirect(redirect[urlParts[0]], 301)
   }
+  if (urlParts[0] == 'g') {
+    switch (urlParts.length) {
+      case 1:
+        return Response.redirect(GITHUB_URL, 301)
+      case 2:
+        return Response.redirect(`${GITHUB_URL}/${urlParts[1]}`, 301)
+      case 3:
+        return Response.redirect(
+          `${GITHUB_URL}/${urlParts[1]}/commit/${urlParts[2]}`,
+          301
+        )
+      case 4:
+        return Response.redirect(
+          `${GITHUB_URL}/${urlParts[1]}/issues/${urlParts[3]}`,
+          301
+        )
+    }
+  }
+  //prevent existing url from breaking
+  if (urlParts[0] == 'vit-curriculum') {
+    Response.redirect(
+      'https://drive.google.com/file/d/1KUBodSJGrW1JaObGYJVENxfTvbvpPZOk/view?usp=sharing',
+      301
+    )
+  }
+  return getPageFromKV(event)
 }
