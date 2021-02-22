f21ea69a567962bb06631e350c45de2643aea531
SaurusXI <vermashantanu@hotmail.com>
2021-02-22 21:11:20 +0530

    fix: dscid to all lowercase

--- workers-site/data.js
+++ workers-site/data.js
@@ -1,6 +1,6 @@
 export const redirect = {
   afterhours: 'https://afterhours.dscvit.com',
-  DSCid: 'https://medium.com/gdg-vit/how-to-handle-imbalanced-datasets-d33d10678c8a?source=friends_link&sk=45f724429b0dc51eb2846bf05c18af9f',
+  dscid: 'https://medium.com/gdg-vit/how-to-handle-imbalanced-datasets-d33d10678c8a?source=friends_link&sk=45f724429b0dc51eb2846bf05c18af9f',
   community: 'https://dsc.community.dev/vellore-institute-of-technology/',
   insta: 'https://www.instagram.com/dscvitvellore',
   linkedin: 'https://www.linkedin.com/company/dscvit',
