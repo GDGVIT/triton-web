735a80398357561a8b4031ae6fcb102d37df84f5
Astha Jha <67831237+asthajha0211@users.noreply.github.com>
2021-03-13 14:32:53 +0530

    WT'21 Airmeet link

--- workers-site/data.js
+++ workers-site/data.js
@@ -18,5 +18,6 @@ export const redirect = {
   'vit-curriculum':'https://drive.google.com/file/d/1KUBodSJGrW1JaObGYJVENxfTvbvpPZOk/view?usp=sharing',
   'wt21-policy': 'https://dscv.it/scem23.md',
   wtandroid:'https://play.google.com/store/apps/details?id=com.dscvit.wt21',
-  wtios:'https://apps.apple.com/in/app/womentechies-2021/id1556003243#?platform=iphone'
+  wtios:'https://apps.apple.com/in/app/womentechies-2021/id1556003243#?platform=iphone',
+  amwt21: 'https://www.airmeet.com/e/6ad03460-83d3-11eb-86eb-b798d27772c4',
 }
