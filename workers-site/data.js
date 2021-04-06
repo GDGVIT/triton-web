1c495b2757a9e6dd288d3dbcc6ae43a79eebaedb
BRO3886 <siddverma1999@gmail.com>
2021-04-07 00:41:51 +0530

    chore: remove unrequired redirects

--- workers-site/data.js
+++ workers-site/data.js
@@ -1,6 +1,7 @@
 export const redirect = {
   afterhours: 'https://afterhours.dscvit.com',
-  dscid: 'https://medium.com/gdg-vit/how-to-handle-imbalanced-datasets-d33d10678c8a?source=friends_link&sk=45f724429b0dc51eb2846bf05c18af9f',
+  dscid:
+    'https://medium.com/gdg-vit/how-to-handle-imbalanced-datasets-d33d10678c8a?source=friends_link&sk=45f724429b0dc51eb2846bf05c18af9f',
   community: 'https://dsc.community.dev/vellore-institute-of-technology/',
   insta: 'https://www.instagram.com/dscvitvellore',
   linkedin: 'https://www.linkedin.com/company/dscvit',
@@ -9,22 +10,12 @@ export const redirect = {
   solutionchallengeinfo:
     'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-solution-challenge-info-session/#/',
   twitter: 'https://twitter.com/dscvit',
-  vitcourses:
-    'https://drive.google.com/file/d/1KUBodSJGrW1JaObGYJVENxfTvbvpPZOk/view?usp=sharing',
-  wt21: 'https://womentechies.dscvit.com/',
-  wtdiscord: 'https://discord.gg/PvkaBF7yp4',
   youtube: 'https://www.youtube.com/channel/UCvT-ZJF7fXHJi9kDeCPR-zg',
-  resources: 'https://resources.dscvit.com' ,
-  'vit-curriculum':'https://drive.google.com/file/d/1KUBodSJGrW1JaObGYJVENxfTvbvpPZOk/view?usp=sharing',
-  'wt21-policy': 'https://dscv.it/scem23.md',
-  wtandroid:'https://play.google.com/store/apps/details?id=com.dscvit.wt21',
-  wtios:'https://apps.apple.com/in/app/womentechies-2021/id1556003243#?platform=iphone',
-  amwt21: 'https://www.airmeet.com/e/6ad03460-83d3-11eb-86eb-b798d27772c4',
-  wtcheckin: 'https://vithack2020.typeform.com/to/KxDhQ3qP',
-  wt21checkin: 'https://vithack2020.typeform.com/to/KxDhQ3qP',
+  resources: 'https://resources.dscvit.com',
   discord: 'https://discord.gg/4VqH3RJQXG',
-  firebase:'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-introduction-to-firebase/#/',
-  gsoc: 'https://go.vitspot.com/mq9',
-  ep6reg: 'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-google-summer-of-code-experience-and-tips-dsc-afterhours/',
-  'org-monster':'https://chrome.google.com/webstore/detail/org-monster/egilbonoajnapajkbihnalficajlomcn',
+  firebase:
+    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-introduction-to-firebase/#/',
+  ep6reg:
+    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-google-summer-of-code-experience-and-tips-dsc-afterhours/',
+  'org-monster': 'https://chrome.google.com/webstore/detail/org-monster/egilbonoajnapajkbihnalficajlomcn',
 }
