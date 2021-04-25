a95b5d7e87a5f062c00d20ee5ff34dd47c5b3eb1
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-25 23:36:47 +0530

    chore: edit redirects

--- workers-site/data.js
+++ workers-site/data.js
@@ -5,19 +5,10 @@ export const redirect = {
   community: 'https://dsc.community.dev/vellore-institute-of-technology/',
   insta: 'https://www.instagram.com/dscvitvellore',
   linkedin: 'https://www.linkedin.com/company/dscvit',
-  solchal:
-    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-solution-challenge-info-session/#/',
-  solutionchallengeinfo:
-    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-solution-challenge-info-session/#/',
   twitter: 'https://twitter.com/dscvit',
   youtube: 'https://www.youtube.com/channel/UCvT-ZJF7fXHJi9kDeCPR-zg',
   resources: 'https://resources.dscvit.com',
   discord: 'https://discord.gg/4VqH3RJQXG',
-  firebase:
-    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-introduction-to-firebase/#/',
-  ep6reg:
-    'https://dsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-google-summer-of-code-experience-and-tips-dsc-afterhours/',
   'org-monster': 'https://chrome.google.com/webstore/detail/org-monster/egilbonoajnapajkbihnalficajlomcn',
-  python: 'https://dsc.community.dev/e/m8dpk3/',
-  devcamp2: 'https://dsc.community.dev/e/m4ht92/'
+  camp2: 'https://dsc.community.dev/e/m4ht92/'
 }
