2898ec08707e0f0cacf8bcd136b459af20acf5f7
vishal-1408 <chinthamvishal.2019@vitstudent.ac.in>
2021-08-18 12:48:43 +0530

    chore: remove blog from shortlinks of mediumRedirect

--- workers-site/data.js
+++ workers-site/data.js
@@ -48,18 +48,18 @@ export const mediumRedirect = {
   dscid:
   'https://medium.com/gdg-vit/how-to-handle-imbalanced-datasets-d33d10678c8a?source=friends_link&sk=45f724429b0dc51eb2846bf05c18af9f',
   minimalism: 'https://medium.com/@aroranamya5/dial-down-the-minimalism-77f8dc0d9641',
-  blogonlinecollege:
+  onlinecollege:
   'https://medium.com/gdg-vit/pandemic-online-classes-everything-else-5a483d1423e5?source=friends_link&sk=877ec26afabaab4dde97444f9bec6f5c',
-  mlrandomnessblog:
+  mlrandomness:
   'https://medium.com/gdg-vit/understanding-the-role-of-randomness-in-machine-learning-4a19632f41c4?source=friends_link&sk=4714e51d2e4c1b060d8e593c9b4c2a8f',
-  getstartedblog:
+  getstarted:
   'https://medium.com/gdg-vit/5-tips-to-get-you-started-developeredition-bccfc2adf9ff?source=friends_link&sk=7cb75db17e668baaced0c1815b643bdd',
   notebooksmaster:
   'https://medium.com/gdg-vit/my-kaggle-journey-from-novice-to-notebooks-masters-92bb4dc51a89',
   songifiedandcyanite:
   'https://medium.com/gdg-vit/songified-and-cyanite-1cdb187b4168?source=friends_link&sk=f8ea2324bdf0cde8f1194f3388ea4ce6',
   searchengine:'https://medium.com/gdg-vit/a-guide-to-understanding-search-engines-b83f12132722',
-  notifierblog:'https://medium.com/gdg-vit/notifier-bot-be892baaf445?source=friends_link&sk=57c01e86a29f3d79c29d3ffaeb883c44',
-  backendblog:'https://medium.com/gdg-vit/which-backend-stack-should-you-learn-e4935dfe81bd?source=friends_link&sk=166633a73e76a8e7328420d594f1c798',
-  tensorflowblog:'https://medium.com/gdg-vit/my-journey-of-becoming-a-tensorflow-certified-developer-9ba6134807b5?source=friends_link&sk=8dfe2284188ae7ab62cc5142eaf2a631'
+  notifier:'https://medium.com/gdg-vit/notifier-bot-be892baaf445?source=friends_link&sk=57c01e86a29f3d79c29d3ffaeb883c44',
+  backend:'https://medium.com/gdg-vit/which-backend-stack-should-you-learn-e4935dfe81bd?source=friends_link&sk=166633a73e76a8e7328420d594f1c798',
+  tensorflow:'https://medium.com/gdg-vit/my-journey-of-becoming-a-tensorflow-certified-developer-9ba6134807b5?source=friends_link&sk=8dfe2284188ae7ab62cc5142eaf2a631'
 }
