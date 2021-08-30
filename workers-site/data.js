d8b432ac215613ce8b60fc9b510f4ece731409ad
Astha Jha <67831237+asthajha0211@users.noreply.github.com>
2021-08-30 19:09:31 +0530

    chore: update camp8

--- workers-site/data.js
+++ workers-site/data.js
@@ -45,4 +45,5 @@ export const redirect = {
   camp6: 'https://gdsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-presents-workshop-on-advanced-react/',
   hackjudgeblog:'https://medium.com/gdg-vit/hackjudge-from-inception-to-application-af92c0221f04?source=friends_link&sk=df3f0f46ce03ecc9e00be7239aeabd85',
   camp7:'https://gdsc.community.dev/e/mn2qht/',
+  camp8:'https://gdsc.community.dev/e/mz6uzv/',
 }
