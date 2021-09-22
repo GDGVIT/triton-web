b8177118264335f78cff63422c07c98aefba7d9d
Astha Jha <67831237+asthajha0211@users.noreply.github.com>
2021-09-22 15:51:56 +0530

    chore: add GCPtelegram redirect

--- workers-site/data.js
+++ workers-site/data.js
@@ -49,4 +49,5 @@ export const redirect = {
   camp8:'https://gdsc.community.dev/e/mz6uzv/',
   pipoblog:'https://medium.com/gdg-vit/pipo-the-journey-from-ideation-to-creation-78e91e1e892d?source=friends_link&sk=ee4abd91c78841c87418ea39014c7ee6',
   camp9:'https://gdsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-vit-vellore-presents-intro-to-ml-devcamp/',
+  GCPtelegram:'https://t.me/joinchat/nFUPhgOIUbQ5OTQ1',
 }
