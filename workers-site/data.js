7996d7c09bdb44f668c61aafd2edafb5a840a83e
Astha Jha <67831237+asthajha0211@users.noreply.github.com>
2022-01-13 14:55:57 +0700

    chore: add dj21build redirect

--- workers-site/data.js
+++ workers-site/data.js
@@ -66,5 +66,6 @@ export const redirect = {
   asj2:'https://gdsc.community.dev/e/mrzrrb/',
   asj3:'https://gdsc.community.dev/e/mn929x/',
   dnblog:'https://medium.com/@kushojha1209/e95287253c86?source=friends_link&sk=3e9ac615eaec7f9f2abf4e1459b15925',
+  dj21build:'https://gdsc.community.dev/e/m4jtwh/',
 
 }
