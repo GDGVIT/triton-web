9777fe8e36389cf423d98c4b17b9e889ba53d479
Yajat Malhotra <68477362+iamyajat@users.noreply.github.com>
2022-01-20 12:42:11 +0530

    chore: vitty website link added

--- workers-site/data.js
+++ workers-site/data.js
@@ -38,6 +38,7 @@ export const redirect = {
     'https://play.google.com/store/apps/details?id=com.dscvit.vitty',
   'vitty-extension':
     'https://chrome.google.com/webstore/detail/vitty/eeohmkjefmpmddidkjadpifbfcplkifh',
+  vitty: 'https://vitty.dscvit.com',
   ep6reg:'https://www.youtube.com/watch?v=RV_24cI6GJE&feature=youtu.be',
   searchengine:'https://medium.com/gdg-vit/a-guide-to-understanding-search-engines-b83f12132722',
   notifierblog:'https://medium.com/gdg-vit/notifier-bot-be892baaf445?source=friends_link&sk=57c01e86a29f3d79c29d3ffaeb883c44',
