71640056a3997495328b493db25f9beff713d256
Astha Jha <67831237+asthajha0211@users.noreply.github.com>
2022-07-21 13:18:33 +0530

    chore: added blog csec

--- workers-site/data.js
+++ workers-site/data.js
@@ -72,4 +72,5 @@ export const redirect = {
   wtrev2:'https://us02web.zoom.us/j/82154603017?pwd=VDlMSWtMZURoZEdhSDNPMGNxeTFwUT09',
   aiblog: 'https://medium.com/@rtrivedi1743/ai-automation-and-the-future-of-workplaces-65de5d04c475?source=friends_link&sk=4d7351e6dca97b6eda1aa92d92a16b72',
   typeblog: "https://medium.com/@aastha2003gupta/typography-and-its-impact-on-graphic-design-a1bf3bdd62a0",
+  b-csec: 'https://medium.com/gdg-vit/deciphering-cybersecurity-essentials-88f59b787572',
 }
