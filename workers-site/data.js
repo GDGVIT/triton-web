516c60eb90c7ff925d5a46a185854de48c6162a6
Dhruv Shah <88224695+Dhruv9449@users.noreply.github.com>
2024-04-09 18:48:58 +0530

    fix: crm-blog link

--- workers-site/data.js
+++ workers-site/data.js
@@ -140,5 +140,5 @@ export const redirect = {
   'ff-writer':'https://ffwriter.dscvit.com',
   'duolingo-blog':'https://medium.com/@eshasantosh/decoding-duolingo-how-digital-design-can-shape-language-learning-journeys-8a37f48138fc',
   'r2-tech':'https://docs.google.com/document/d/1-jr9H2qxobrJVGXPX26R0XOS4zj9OYXJ5nZ1CN978yE/edit?usp=sharing',
-  'CRM-blog':'https://medium.com/gdg-vit/crafting-customer-connections-exploring-the-realm-of-crm-57be3ea1df18'
+  'crm-blog':'https://medium.com/gdg-vit/crafting-customer-connections-exploring-the-realm-of-crm-57be3ea1df18'
 }
