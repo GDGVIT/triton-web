0c5e3f451c7e8892aaee6cbb54837a49c09d3f3b
Harsh Shah <71080727+devHarshShah@users.noreply.github.com>
2024-07-29 18:24:19 +0530

    feat:added landing page with text areas

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,6 +1,13 @@
+"use client";
+import { useState } from 'react';
 import Image from 'next/image';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
   return (
     <nav className="bg-background flex items-center justify-between p-4 mx-4 font-jetbrains-mono">
       <div className="flex items-center">
@@ -13,12 +20,31 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div className="flex items-center space-x-8">
-        <button className="bg-white text-black px-4 py-1 rounded-2xl hover:bg-secondary">Analytics</button>
-        <button className="text-muted hover:text-muted-foreground">
-          <Image alt="save icon" src="/save.svg" width={25} height={25} />
-        </button>
+      <div className="flex items-center space-x-2">
+        <div className="hidden md:flex items-center space-x-8">
+          <button className="bg-white text-black px-4 py-1 rounded-2xl hover:bg-secondary">Analytics</button>
+          <button className="text-muted hover:text-muted-foreground flex items-center space-x-2">
+            <Image alt="save icon" src="/save.svg" width={25} height={25} />
+            <span>Save</span>
+          </button>
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-muted hover:text-muted-foreground">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden absolute top-16 right-4 bg-background border border-gray-700 rounded-lg shadow-lg p-4 bg-black">
+          <button className="block w-full text-left  text-white px-4 py-2 rounded-lg hover:bg-secondary mb-2">Analytics</button>
+          <button className=" w-full text-left text-muted hover:text-muted-foreground flex items-center space-x-2">
+            <Image alt="save icon" src="/save.svg" width={25} height={25} />
+            <span>Save</span>
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,19 @@
-import Image from "next/image";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-export default function Home() {
+const Page = () => {
   return (
     <>
+      <Navbar />
+      <div className="flex flex-col md:flex-row h-[85vh] font-jetbrains-mono">
+        <textarea className="w-full md:w-1/2 h-full resize-none p-4 bg-[#212121] focus:outline-none" placeholder="Pastebin and URL shortener. Paste, save and share!"></textarea>
+        <div className="w-full h-0.5 md:w-0.5  md:h-full bg-black"></div>
+        <textarea className="w-full md:w-1/2 h-full resize-none p-4 mb-10 bg-[#212121] focus:outline-none" placeholder="Pastebin and URL shortener. Paste, save and share!" readOnly></textarea>{' '}
+      </div>
+      <Footer />
     </>
   );
-}
+};
+
+export default Page;
