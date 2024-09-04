797a0be7a63101c3a564fcd6a99c304db4288b1e
AdityaSareen06 <63975070+AdityaSareen06@users.noreply.github.com>
2024-09-04 20:38:12 +0530

    chore : mapped hacked mother blog

--- workers-site/data.js
+++ workers-site/data.js
@@ -150,6 +150,6 @@ export const redirect = {
   'quantum-tales-blog':'https://medium.com/gdg-vit/the-quantum-tale-dc12f1df8b22',
   'devjams-blog':'https://medium.com/gdg-vit/the-hackathon-hustle-a-devjams-starter-pack-0ee9f6efa932',
   'xr-blog':'https://medium.com/gdg-vit/redefining-reality-an-xr-guide-7f876c89c8ef',
-  'maps-blog':'https://medium.com/gdg-vit/mapping-the-magic-bts-with-google-maps-d4db92990cba'
+  'maps-blog':'https://medium.com/gdg-vit/mapping-the-magic-bts-with-google-maps-d4db92990cba',
   'hacked-blog':'https://medium.com/gdg-vit/how-i-hacked-into-my-moms-browser-80162e79754e'
 }
