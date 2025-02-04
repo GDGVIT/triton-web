6b05cd1dec3ff31e75a094683becfedc35c6972c
Aditya Chowdhary <adityachowdhary04@gmail.com>
2025-02-05 02:26:39 +0530

    fix: removed timestamp in hex-marketplace link

--- workers-site/data.js
+++ workers-site/data.js
@@ -158,6 +158,6 @@ export const redirect = {
   'flutter-session': 'https://www.youtube.com/watch?v=Q6d2PAENuuc',
   'sdg-session':'https://www.youtube.com/watch?v=G0B-MNeWGSs',
   'wt-brochure':'https://drive.google.com/file/d/1jnOQDXYv2wxAJ4T662NLXWCyTzWgPooZ/view',
-  'hex-marketplace':'https://www.youtube.com/watch?v=TgsxcuPeASw&t=27s&ab_channel=GDSCVIT',
+  'hex-marketplace':'https://www.youtube.com/watch?v=TgsxcuPeASw&ab_channel=GDSCVIT',
   'hex-discord':'https://discord.gg/tfHqRNpc'
 }
